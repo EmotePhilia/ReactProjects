68a14676af9dcc8581b33c11ab051b520779411d
EmotePhilia <szymonkda@gmail.com>
2022-11-15 15:46:37 +0100

    Dodany footer

--- Business_Card/index.js
+++ Business_Card/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Nav from "./components/Nav.js";
 import Main from "./components/Main.js";
+import Footer from "./components/Footer.js";
 function App(){
     return(
         <div id="page-wrapper">
