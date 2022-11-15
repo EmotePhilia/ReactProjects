cfc3fa13e461fc786ba339cbd91ab6d4823e2aa8
EmotePhilia <szymonkda@gmail.com>
2022-11-15 15:40:02 +0100

    Dodany folder do business card (musiałem mieć stary folder pobrany bez footera)

---
+++ Business_Card/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Footer(){
+    return(
+        <footer>
+            <a href="https://www.facebook.com/"><img src="./images/facebook.svg" className="footer-icons" id="fb" alt="facebook"></img></a>
+            <a href="https://www.github.com"><img src="./images/github2.svg" className="footer-icons"  id="gh" alt="github"></img></a>
+            <a href="https://www.instagram.com"><img src="./images/instagram.svg" className="footer-icons" id="ig" alt="instagram"></img></a>
+            <a href="https://www.linkedin.com"><img src="./images/linkedin.svg" className="footer-icons" id="in" alt="linkedin"></img></a>
+        </footer>
+    )   
+}
--- Business_Card/index.js
+++ Business_Card/index.js
@@ -7,6 +7,7 @@ function App(){
         <div id="page-wrapper">
         <Nav />
         <Main />
+        <Footer />
         </div>
     )
 }
